2cf9f717784daafd923500a4e8e84ebeaa796a83
Dmitrijs Mihailovs <dimka62@inbox.lv>
2017-11-16 18:12:58 +0200

    Menu seeder update

--- Database/Seeders/MenusTableSeeder.php
+++ Database/Seeders/MenusTableSeeder.php
@@ -5,7 +5,7 @@ namespace Modules\Classified\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Admin\Models\Menu;
-use Modules\Admin\Models\MenuItem;
+use Netcore\Translator\Helpers\TransHelper;
 
 class MenusTableSeeder extends Seeder
 {
@@ -57,16 +57,32 @@ class MenusTableSeeder extends Seeder
                 'name' => $name
             ]);
 
-
             foreach ($items as $item) {
-                $item['menu_id'] = $menu->id;
-                $parentItem = MenuItem::firstOrCreate(array_except($item, 'children'));
+                $row = $menu->items()->firstOrCreate(array_except($item, ['name', 'value', 'parameters', 'children']));
 
-                foreach($item['children'] as $child) {
-                    $child['parent_id'] = $parentItem->id;
+                $translations = [];
+                foreach (TransHelper::getAllLanguages() as $language) {
+                    $translations[$language->iso_code] = [
+                        'name'       => $item['name'],
+                        'value'      => $item['value'],
+                        'parameters' => $item['parameters']
+                    ];
+                }
+                $row->updateTranslations($translations);
+
+                foreach ($item['children'] as $child) {
                     $child['menu_id'] = $menu->id;
 
-                    MenuItem::firstOrCreate($child);
+                    $c = $row->children()->firstOrCreate(array_except($child, ['name', 'value', 'parameters']));
+                    $translations = [];
+                    foreach (TransHelper::getAllLanguages() as $language) {
+                        $translations[$language->iso_code] = [
+                            'name'       => $child['name'],
+                            'value'      => $child['value'],
+                            'parameters' => $child['parameters']
+                        ];
+                    }
+                    $c->updateTranslations($translations);
                 }
             }
         }
